7176b470fa02b978a8f1098d2159a8686598ff5e
Khalil Fadel <khalilfadel02@gmail.com>
2025-10-06 18:20:11 -0300

    Subindo projeto Spring Boot - Crud

---
+++ src/main/java/com/shaulin/crud/repositories/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.shaulin.crud.repositories;
+import com.shaulin.crud.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+}
